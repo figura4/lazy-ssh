6df4294a00bd8d938bcb591ccd05ab3360f094f0
oscar.riva <sezione9@gmail.com>
2014-01-15 17:44:58 +0100

    drawer icons change working

--- LazySsh/src/com/figura4/lazyssh/adapters/DrawerArrayAdapter.java
+++ LazySsh/src/com/figura4/lazyssh/adapters/DrawerArrayAdapter.java
@@ -8,16 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class DrawerArrayAdapter extends ArrayAdapter<String> {
+public class DrawerArrayAdapter extends BaseAdapter {
 	private final Context context;
 	private final String[] values1;
 	private final String[] values2;
 
 	public DrawerArrayAdapter(Context context, String[] values1, String[] values2) {
-	    super(context, R.layout.drawer_list_item, values1);
 	    this.context = context;
 	    this.values1 = values1;
 	    this.values2 = values2;
@@ -34,8 +34,26 @@ public class DrawerArrayAdapter extends ArrayAdapter<String> {
     	
     	textView1.setText(values1[position]);
     	textView2.setText(values2[position]);
-    	int identifier = context.getResources().getIdentifier(values2[position], "drawable", "com.figura4.lazyssh");
+    	
+    	Log.d("lazyssh", "R.drawable.ic_action_settings: "+ R.drawable.ic_action_settings);
+    	Log.d("lazyssh", "getIdentifier():" + context.getResources().getIdentifier(values2[position], "drawable", context.getPackageName()));
+    	int identifier = context.getResources().getIdentifier(values2[position], "drawable", context.getPackageName());
     	imageView.setImageResource(identifier);
     	return rowView;
     }
+
+	@Override
+	public int getCount() {
+		return values1.length;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return values1[position];
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
 }
